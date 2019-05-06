a014efef39129d520e4e2cfeed8b3b77b49fb316
Michal Mojek <m.mojek@gmail.com>
2019-05-06 15:52:32 +0200

    Uniqueness of races

--- race-results-service/api/models.py
+++ race-results-service/api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Race(models.Model):
     name = models.CharField(max_length=256)
-    start_date = models.DateField(null=True, blank=True)
+    start_date = models.DateField()
     distance = models.DecimalField(
         max_digits=10, decimal_places=1, null=True, blank=True
     )
@@ -14,6 +14,11 @@ class Race(models.Model):
     time_limit = models.DecimalField(
         max_digits=10, decimal_places=1, null=True, blank=True
     )
+
+    class Meta:
+        ordering = ["-start_date"]
+        verbose_name_plural = "races"
+        unique_together = ["name", "start_date"]
 
     def __str__(self):
         return f"{self.name} {self.start_date}"
--- race-results-service/api/tests/test_model.py
+++ race-results-service/api/tests/test_model.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.core.exceptions import ValidationError
+from django.db.utils import IntegrityError
 from api import models
 
 
@@ -21,7 +22,7 @@ class ModelTest(TestCase):
     def test_elevation_diff(self):
         """Test elevation diff calculation """
         race = models.Race.objects.create(
-            elevation_gain=1925, elevation_lost=1925
+            start_date="2019-04-27", elevation_gain=1925, elevation_lost=1925
         )
         self.assertEqual(
             race.elevation_diff, (race.elevation_gain - race.elevation_lost)
@@ -29,8 +30,11 @@ class ModelTest(TestCase):
 
     def test_elevation_gain_validation(self):
         """Test validation of elevation gain is >= 0"""
-        race = models.Race.objects.create(
-            name="Prehyba", elevation_gain=-10, elevation_lost=1925
+        race = models.Race(
+            start_date="2019-04-27",
+            name="Prehyba",
+            elevation_gain=-10,
+            elevation_lost=1925,
         )
 
         with self.assertRaisesRegexp(ValidationError, "elevation_gain"):
@@ -38,9 +42,28 @@ class ModelTest(TestCase):
 
     def test_elevation_lost_validation(self):
         """Test validation of elevation lost is >= 0"""
-        race = models.Race.objects.create(
-            name="Prehyba", elevation_gain=10, elevation_lost=-25
+        race = models.Race(
+            name="W Prehyba",
+            start_date="2019-04-27",
+            elevation_gain=10,
+            elevation_lost=-25,
         )
 
         with self.assertRaisesRegexp(ValidationError, "elevation_lost"):
+            race.full_clean()
+
+    def test_uniqness_of_the_races(self):
+        """Test uniqness of the races.
+            Race with same name and date on database"""
+        models.Race.objects.create(name="Prehyba", start_date="2019-04-27")
+        with self.assertRaises(IntegrityError):
+            models.Race.objects.create(name="Prehyba", start_date="2019-04-27")
+
+    def test_uniqness_of_the_races_django(self):
+        """Test uniqness of the races.
+            Race with same name and date on django"""
+        models.Race.objects.create(name="Prehyba", start_date="2019-04-27")
+        race = models.Race(name="Prehyba", start_date="2019-04-27")
+
+        with self.assertRaises(ValidationError):
             race.full_clean()
