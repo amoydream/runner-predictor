93b8795f089fa87d8759286b4c1787a89d34ca22
Michal Mojek <m.mojek@gmail.com>
2019-09-04 11:24:42 +0200

    Some style update

--- front-vue-service/src/components/Shared/Header.vue
+++ front-vue-service/src/components/Shared/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-light bg-light">
+  <nav class="navbar navbar-light bg-light mx-auto" style="max-width:800px;">
     <span class="navbar-brand mb-0 h1">Runner Predictor</span>
   </nav>
 </template>
